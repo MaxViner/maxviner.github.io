0d9e2a897c652d2fe88508eaceab7c89c2a7ae21
max <kodtya.oreshin@inbox.ru>
2024-07-10 18:45:49 +0300

    depl

---
+++ src/const/const.js
@@ -0,0 +1,324 @@
+export const studies = [
+  {
+    title: "obtaining specialization on the online platform ",
+    corporation: "GeekBrains",
+    date: "08/2022 – 04/2024",
+    img:'../../../img/edy/gb.png'
+  },
+  {
+    title: "Internship at an it startup",
+    corporation: "PREAX",
+    date: "06/2023 – 12/2023",
+    img:'../../../img/edy/preax.jpg'
+  },
+  {
+    title: "Internship at an it company",
+    corporation: "Alpha ecosystem",
+    date: "04/2023 – 05/2024",
+    img:'../../../img/edy/alpha.png'
+  },
+  {
+    title: "Advancement courses from developer to development team leader",
+    corporation: "GeekBrains",
+    date: "05/2024 – ...",
+  },
+];
+
+export const IconKeys = {
+  html: "html",
+  css: "css",
+  javaScript: "javaScript",
+  react: "react",
+  astro: "astro",
+  materialUi: "materialUi",
+  nextjs: "nextjs",
+  redux: "redux",
+  styled: "styled",
+  tailwind: "tailwind",
+  radix: "radix",
+  reactQuery: "reactQuery",
+  typeScript: "typeScript",
+  zustand: "zustand",
+  git: "git",
+  github: "github",
+  stripe: "stripe",
+  shadcn: "shadcn",
+  nodejs: "nodejs",
+  mongodb: "mongodb",
+  vite: "vite",
+  vue: 'vue',
+  // leaflet:'leaflet',
+};
+
+export const skills = [
+  {
+    title: "Html",
+    icon: IconKeys.html,
+  },
+  {
+    title: "Css",
+    icon: IconKeys.css,
+  },
+  {
+    title: "JavaScript",
+    icon: IconKeys.javaScript,
+  },
+  {
+    title: "Astro",
+    icon: IconKeys.astro,
+  },
+  {
+    title: "Tailwindcss",
+    icon: IconKeys.tailwind,
+  },
+  {
+    title: "React Js",
+    icon: IconKeys.react,
+  },
+  {
+    title: "Next Js",
+    icon: IconKeys.nextjs,
+  },
+  {
+    title: "Git",
+    icon: IconKeys.git,
+  },
+
+  {
+    title: "Github",
+    icon: IconKeys.github,
+  },
+  {
+    title: "Material UI",
+    icon: IconKeys.materialUi,
+  },
+
+  {
+    title: "Redux Js",
+    icon: IconKeys.redux,
+  },
+  {
+    title: "Styled Components",
+    icon: IconKeys.styled,
+  },
+  {
+    title: "Node Js",
+    icon: IconKeys.nodejs,
+  },
+  {
+    title: "TypeScript",
+    icon: IconKeys.typeScript,
+  },
+  {
+    title: "Vue js",
+    icon: IconKeys.vue,
+  },
+
+  {
+    title: "React Query",
+    icon: IconKeys.reactQuery,
+  },
+  {
+    title: "Vite",
+    icon: IconKeys.vite,
+  },
+  {
+    title: "Zustand",
+    icon: IconKeys.zustand,
+  },
+
+  // {
+  //   title: "Shadcn UI",
+  //   icon: IconKeys.shadcn,
+  // },
+  // {
+  //   title: "Mongo Db",
+  //   icon: IconKeys.mongodb,
+  // },
+];
+
+export const projects = [
+  {
+    img: "../../../img/hall.png",
+    title: "Hall of Fame",
+    link: "https://champbook-pi.vercel.app/",
+    description: `Collection of quotes from great people `,
+    technologies: [
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.react,
+      },
+      {
+        icon: IconKeys.redux,
+      },
+      {
+        icon: IconKeys.github,
+      },
+      {
+        icon: IconKeys.css,
+      },
+    ],
+  },
+  {
+    img: "../../../img/folio.png",
+    title: "portfolio-template",
+    link: "https://realityvirtual.netlify.app/",
+    description: `Portfolio created without using js frameworks`,
+    technologies: [
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.html,
+      },
+    ],
+  },
+  {
+    img: "../../../img/kat.png",
+    title: "Personal Website",
+    link: "https://master--voluble-jelly-e32c4c.netlify.app/",
+    description: `Portfolio website created for a manicurist`,
+    technologies: [
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.react,
+      },
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+
+    ],
+  },
+  {
+    img: "../../../img/gym.png",
+    title: "GymLanding",
+    link: "https://anveraly-group-test.vercel.app/",
+    description: `test task to start an internship at an IT company`,
+    technologies: [
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+    ],
+  },
+  {
+    img: "../../../img/weather.png",
+    title: "Weather App",
+    link: "https://maxviner.github.io/",
+    description: `Web application for viewing weather forecasts by city`,
+    technologies: [
+      {
+        icon: IconKeys.react,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.git,
+      },
+    ],
+  },
+  {
+    img: "../../../img/gryz.png",
+    title: "Freight Website",
+    link: "https://gryzoperevozki-ahtubinsk.vercel.app/",
+    description: `Website created for the purpose of advertising an entrepreneur`,
+    technologies: [
+      {
+        icon: IconKeys.react,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.html,
+      },
+    ],
+  },
+  {
+    img: "../../../img/interno.png",
+    title: "vue project",
+    link: "https://my-vue-to-geekbrains.vercel.app/#/blog",
+    description: `Online store website created as part of studying the Vue framework at geekBrains courses`,
+    technologies: [
+      {
+        icon: IconKeys.vite,
+      },
+      {
+        icon: IconKeys.vue,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.github,
+      },
+    ],
+  },
+
+  {
+    img: "../../../img/store.png",
+    title: "Web Store",
+    link: "https://webstore-zeta.vercel.app/",
+    description: `Web store website template using simple html + js `,
+    technologies: [
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.git,
+      },
+    ],
+  },
+  {
+    img: "../../../img/sport.png",
+    title: "sport-traker",
+    link: "https://sport-traker.vercel.app/",
+    description: `web application for tracking your progress in training (currently in development)`,
+    technologies: [
+      {
+        icon: IconKeys.nextjs,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.reactQuery,
+      },
+      {
+        icon: IconKeys.zustand,
+      },
+    ],
+  },
+
+];
